0507b935ba4f9ee1ef932a6fca89699230a4131d
strowger <strowger@ls12.net>
2015-06-19 11:48:03 +0100

    update power.pl with better logging create blank rrds for it with 30 year life and better names

--- bin/create-blank-rrds.sh
+++ bin/create-blank-rrds.sh
@@ -9,20 +9,41 @@
 #
 exit 0
 
-#in ds:
+#options to create, in order left to right:
 #start time when the s/w was first used
-#step time in seconds
-#counter/gauge type
-#time without a value before entering "nothing" in rrd
-#min value
-#max value
+#step time in seconds (how often we record a value)
+#DS - name, counter/gauge type, time without a value before entering "nothing" in rrd, min value, max value
 #
-#in rra:
-#0.5 - can't remember
+#RRA - 0.5 - can't remember
 #next - how many seconds between records
 #how many records to keep
 
 # TODO aren't these MIN/MAX all arse? 1440 = 24mins, 3650 times?
 
-rrdtool create 
+# we want 30 years of data
+# once per minute = 1440 per day, 527040 per (366 day)year, 15811200 per 30 years
+
+## 20150619 styes
+## 20101009 original
+# currentcost
+# outputs a value in watts and a temperature (of the receiver) every 6 seconds
+# we'll capture them all but only save one per minute 
+# we have two different data sources - the clamp meter and the pulse counter
+# the clamp meter is less accurate and gives a number of watts
+# the pulse counter gives a number of pulses as well as a calculated number of watts
+rrdtool create cctemp.rrd --start 1434700000 --step 60 \
+DS:temp:GAUGE:120:-10:110 \
+RRA:LAST:0.5:1:15811200
+
+rrdtool create ccclampwatts.rrd --start 1434700000 --step 60 \
+DS:power:GAUGE:120:0:10000 \
+RRA:LAST:0.5:1:15811200
+
+rrdtool create ccoptiwatts.rrd --start 1434700000 --step 60 \
+DS:power:GAUGE:120:0:10000 \
+RRA:LAST:0.5:1:15811200
+
+rrdtool create ccopticount.rrd --start 1434700000 --step 60 \
+DS:power:COUNTER:120:0:1000 \
+RRA:LAST:0.5:1:15811200
 
--- bin/power.pl
+++ bin/power.pl
@@ -6,16 +6,23 @@
 # 20130312
 # add 'optismart' meter
 # 20150619 GH
-# move to styes, alter device name, paths
+# move to styes, alter device name, paths, tidy up logging
+
+# log everything we get in the temp log, delete it at startup
+# log the values we capture in the power log and keep forever
 
 $logdirectory="/data/hm/log";
+$templogfile="powertemp.log";
 $logfile="power.log";
 $rrddirectory="/data/hm/rrd";
+
+# relies on udev having been configured to create a /dev/currentcost
+# pointing at the correct serial device and with suitable permissions
 
 `stty -F /dev/currentcost 57600`;
 `stty -F /dev/currentcost oddp`;
 
-# the old logfile only held clamp meter data
+open TEMPLOG, ">", "$logdirectory/$templogfile" or die $!;
 open LOGFILE, "+>>", "$logdirectory/$logfile" or die $!;
 open SERIAL, "<", "/dev/currentcost" or die $!;
 
@@ -25,32 +32,40 @@ while (1) {
 while ($line = <SERIAL>)
 {
   $timestamp = time();
-  print LOGFILE "$timestamp $line";
+  print TEMPLOG "$timestamp: $line";
   # catch only the real-time data and not the historical stuff it chucks out periodically
   # sensor 0 is the clamp meter
   # sensor 8 is the calculated watts value from the optismart??
-  # sensor 9 we ignore, it's the raw value of impulses from the optismart??
+  # sensor 9 is the raw value of impulses from the optismart??
   if ($line =~ m!<tmpr>\s*(-*[\d.]+)</tmpr><sensor>0</sensor>.*<ch1><watts>0*(\d+)</watts></ch1>!)
   {
      $temp = $1;
      $powerclamp = $2;
-     print LOGFILE "Got $powerclamp W $temp C from clamp meter\n";
-##     `rrdtool update $rrddirectory/currentcost.rrd $timestamp:$temp:$powerclamp`;
+     print LOGFILE "$timestamp clamp ${powerclamp}W ${temp}C\n";
+     $output = `rrdtool update $rrddirectory/ccclampwatts.rrd $timestamp:$powerclamp`;
+#     print TEMPLOG "rrdtool said $output\n";
+     $output = `rrdtool update $rrddirectory/cctemp.rrd $timestamp:$temp`;
+#     print TEMPLOG "rrdtool said $output\n";
   }
   if ($line =~ m!<tmpr>\s*(-*[\d.]+)</tmpr><sensor>8</sensor>.*<ch1><watts>0*(\d+)</watts></ch1>!)
   {
      $temp = $1;
      $poweropti = $2;
-     print LOGFILE "Got $poweropti W $temp C from optical meter\n";
-     # no point keeping the temperature again.
-##     `rrdtool update $rrddirectory/currentcostoptical.rrd $timestamp:$poweropti`;	
+     print LOGFILE "$timestamp opti ${poweropti}W ${temp}C\n";
+     $output = `rrdtool update $rrddirectory/ccoptiwatts.rrd $timestamp:$poweropti`;     
+#     print TEMPLOG "rrdtool said $output\n";
+     $output = `rrdtool update $rrddirectory/cctemp.rrd $timestamp:$temp`;
+#     print TEMPLOG "rrdtool said $output\n";
   }
   if ($line =~ m!<tmpr>\s*(-*[\d.]+)</tmpr><sensor>9</sensor>.*<imp>0*(\d+)</imp>!)
   {
     $temp = $1;
     $powerimp = $2;
-    print LOGFILE "Got $powerimp count $temp C from optical meter\n";
-##    `rrdtool update $rrddirectory/currentcostcount.rrd $timestamp:$powerimp`;
+    print LOGFILE "$timestamp opti ${powerimp}cnt ${temp}C\n";
+    $output = `rrdtool update $rrddirectory/ccopticount.rrd $timestamp:$powerimp`;
+#    print TEMPLOG "rrdtool said $output\n";
+    $output = `rrdtool update $rrddirectory/cctemp.rrd $timestamp:$temp`;
+#    print TEMPLOG "rrdtool said $output\n";
     last;
   }
 }
