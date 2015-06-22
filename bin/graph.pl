ff3cf41d9a1e363c0a2aad7bab558aa7e626194c
strowger <strowger@ls12.net>
2015-06-22 06:01:33 +0100

    add boiler temps graphing

--- bin/graph.pl
+++ bin/graph.pl
@@ -20,6 +20,8 @@ open LOCKFILE, ">", $lockfile or die $!;
 
 $output = `rrdtool graph $graphdirectory/temps.png -a PNG -l 0 -y 5:1 --vertical-label "deg c" -s -36h -w 1024 -h 300 'DEF:inside='$rrddirectory/roomtemp.rrd:temp:LAST 'DEF:outside='$rrddirectory/outdoortemp.rrd:temp:LAST 'DEF:boilerrm='$rrddirectory/cctemp.rrd:temp:LAST  'LINE1:inside#ff0000:inside temperature' 'LINE1:outside#0400ff:outside temperature' 'LINE1:boilerrm#00dd00:boiler room temperature' `;
 
+$output = `rrdtool graph $graphdirectory/chtemps.png -a PNG -l 0 -y 10:1 --vertical-label "deg c" -s -36h -w 1024 -h 300 'DEF:cyl='$rrddirectory/cylindertemp.rrd:temp:LAST 'DEF:flow='$rrddirectory/flowtemp.rrd:temp:LAST 'DEF:desflow='$rrddirectory/desiredflowtemp.rrd:temp:LAST 'DEF:modtd='$rrddirectory/modtempdesired.rrd:temp:LAST 'LINE1:cyl#ff0000:cylinder temperature' 'LINE1:flow#0400ff:flow temperature' 'LINE1:desflow#111111:desired flow temperature' 'LINE1:modtd#666666:desired modulation'`;
+
 $output = `rrdtool graph $graphdirectory/power.png -a PNG -l 0 -y 50:5 --vertical-label "watts" -s -36h -w 1024 -h 300 'DEF:optical='$rrddirectory/ccoptiwatts.rrd:power:LAST 'DEF:clamp='$rrddirectory/ccclampwatts.rrd:power:LAST 'LINE1:optical#ff0000:pulse-counter reading' 'LINE1:clamp#0400ff:clamp meter reading'`;
 
 close LOCKFILE;
