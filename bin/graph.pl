54936c9af1c829b24c81bc13cef95e549961442b
strowger <strowger@ls12.net>
2015-06-22 09:50:51 +0100

    graph flame ionisation (alongside modulation state) and water pressure

--- bin/graph.pl
+++ bin/graph.pl
@@ -24,5 +24,10 @@ $output = `rrdtool graph $graphdirectory/chtemps.png -a PNG -l 0 -y 10:1 --verti
 
 $output = `rrdtool graph $graphdirectory/power.png -a PNG -l 0 -y 50:5 --vertical-label "watts" -s -36h -w 1024 -h 300 'DEF:optical='$rrddirectory/ccoptiwatts.rrd:power:LAST 'DEF:clamp='$rrddirectory/ccclampwatts.rrd:power:LAST 'LINE1:optical#ff0000:pulse-counter reading' 'LINE1:clamp#0400ff:clamp meter reading'`;
 
+$output = `rrdtool graph $graphdirectory/waterpressure.png -a PNG -y 0.1:1 --vertical-label "bar" -s -36h -w 1024 -h 300 'DEF:pressure='$rrddirectory/waterpressure.rrd:pres:LAST 'LINE1:pressure#ff0000:water pressure'`;
+
+$output = `rrdtool graph $graphdirectory/ionisationvolts.png -a PNG --vertical-label "volts/modulation percentage" -s -36h -w 1024 -h 300 'DEF:ion='$rrddirectory/ionisationvolts.rrd:volts:LAST 'DEF:modtd='$rrddirectory/modtempdesired.rrd:temp:LAST 'LINE1:ion#ff0000:flame sensor ionisation volts' 'LINE1:modtd#666666:desired modulation'  `;
+
+
 close LOCKFILE;
 unlink $lockfile;
