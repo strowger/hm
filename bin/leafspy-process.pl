5b2eb248ea3cd878dec1ed6867c5817169d0fa0f
strowger <strowger@ls12.net>
2017-02-11 14:47:46 +0000

    leafspy-process.pl: don't process a regenwatts value if regenwh is zero - spurious

--- bin/leafspy-process.pl
+++ bin/leafspy-process.pl
@@ -146,8 +146,9 @@ while (<STDIN>)
   # we have a number of watt-hours and a number of seconds
   #  "watt-hours per hour" are watts, so watt hours per second/3600 are watts
   # don't divide by zero, but set it to something first so we don't have it undefined
+  # if the current value is 0, don't do anything
   $regenwatts = 0;
-  if ( $timediff > 0 )
+  if (( $timediff > 0 ) && ($regenwh > 0))
   {
     $regenwhpersec = $regenwhdiff / $timediff;
     $regenwatts = $regenwhpersec / 3600;
