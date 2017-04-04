1a80f90f903e7f1eaed1e5591f20ad71edd92c16
strowger <strowger@ls12.net>
2017-04-04 20:34:25 +0100

    rtl433-logsync.sh: rsync rtl433 logs from remote box. just runs rsync with locking.

---
+++ bin/rtl433-logsync.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+# 20170404 rsync rtl433 logs from alarmbox
+# 
+# used to be a simple one-liner in a cron job but it has to have locking otherwise
+# rsync processes can accumulate until one or both boxes falls over
+
+if [ -e /tmp/rtl433-logsync-lock ]
+then
+  echo "lockfile exists, aborting"
+  exit 1
+fi
+touch /tmp/rtl433-logsync-lock
+
+rsync strowger@192.168.1.40:/data/hm/rtl/* /data/hm/rtl-in > /data/hm/log/alarmbox-rsync-errors.log 2> /data/hm/log/alarmbox-rsync-errors.log
+
+rm /tmp/rtl433-logsync-lock
+
