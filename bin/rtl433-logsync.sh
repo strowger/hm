7dc7d94786f6110345726a4680ea87e05d8cd337
strowger <strowger@ls12.net>
2017-05-12 07:56:49 +0100

    rtl433-logsync.sh: copy from office machine as well as alarmbox

--- bin/rtl433-logsync.sh
+++ bin/rtl433-logsync.sh
@@ -14,6 +14,8 @@ touch /tmp/rtl433-logsync-lock
 # ignore-missing-args saves a whinge mail/log entry when there are no files to transfer on the source. required an rsync upgrade.
 rsync --remove-source-files --ignore-missing-args strowger@192.168.1.40:/data/hm/rtl/* /data/hm/rtl-in >> /data/hm/log/alarmbox-rsync-errors.log 2>> /data/hm/log/alarmbox-rsync-errors.log
 
+rsync --remove-source-files --ignore-missing-args strowger@192.168.1.41:/data/hm/rtl/* /data/hm/rtl-in >> /data/hm/log/office-rsync-errors.log 2>> /data/hm/log/office-rsync-errors.log
+
 rm /tmp/rtl433-logsync-lock
 
 /data/hm/bin/rtl433-logimport.pl
