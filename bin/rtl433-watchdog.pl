56228ed20e9387adf2f183781cb861db0bb7d0bf
strowger <strowger@ls12.net>
2017-05-25 20:13:07 +0100

    rtl433-watchdog: reboot, after a warning, if we're on the second successive attempt to restart rtl433

--- bin/rtl433-watchdog.pl
+++ bin/rtl433-watchdog.pl
@@ -14,6 +14,7 @@
 
 $templogdirectory="/data/hm/rtltmp";
 $lockfile="/tmp/rtl433-watchdog.lock";
+$logfile="/tmp/rtl-watchdog.log";
 $watchlog="debug.log";
 
 if ( -f $lockfile )
@@ -28,13 +29,28 @@ $timestamp=time();
 $watchctime=(stat("$templogdirectory/$watchlog"))[9];
 $watchage = $timestamp - $watchctime;
 
-open LOGFILE, ">>", "/tmp/rtl-watchdog.log" or die $!;
+$lastlogline = `tail -1 $logfile`;
 
+open LOGFILE, ">>", "$logfile" or die $!;
 
+# this tests for whether the last rtl433 process, which should exit after
+# 60 seconds, has failed to exit
 if ( $watchage > 80 )
 {
   # a temporary log file
   print LOGFILE "$timestamp found $watchlog with age $watchage...";
+  # if the last log line was us killing it for failing to exit, and it's failed
+  # again, it's probably stuck and we'll reboot
+  if ( $lastlogline =~ /killing/ )
+  {
+    print LOGFILE "$timestamp found last watchdog run was a restart, rebooting...\n";
+    `wall "rtl-watchdog found rtl error, rebooting in 1 minute`;
+    close LOGFILE;
+    sleep 90;
+    close LOCKFILE;
+    unlink $lockfile;
+    `sudo reboot`;
+  }
   # if the options used in the rtl433-log script change, have to update this
   $rtlpid = `ps x|grep "/[u]sr/local/bin/rtl_433 -G -U -T 60"|grep -v \\>\\>|awk '{print \$1}'`;
   chomp $rtlpid;
