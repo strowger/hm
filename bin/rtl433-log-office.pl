a68603bd9b8d0c008684274595924b4b25da49fb
strowger <strowger@ls12.net>
2017-07-07 15:16:53 +0100

    rtl433-log: log short (error) sessions, when rtl433 goes wrong somehow

--- bin/rtl433-log-office.pl
+++ bin/rtl433-log-office.pl
@@ -19,6 +19,7 @@ $location="office";
 # -G: enable all protocols
 # -U: use utc for all timestamps
 # -T x: run for x seconds - shouldn't be too short as there's a startup delay
+# ** if these options change, the rtl433-watchdog.pl script must be updated**
 $rtloptions="-G -U -T 60";
 
 
@@ -29,14 +30,28 @@ if ( -f $lockfile )
 }
 open LOCKFILE, ">", $lockfile or die $!;
 
+# sometimes rtl433 goes wrong (hw problem with rtl stick) and exits after a
+# few sessions. we catch those, and count them, with a view to rebooting if
+# it recurrs
+$shortsessions = 0;
+
 while (1)
 {
   $timestamp=time();
+  $starttime=$timestamp;
   open LOGFILE, ">", "$templogdirectory/$location-$timestamp.log" or die $!;
 # there's quite a lot of debug output, which is temporarily useful if it fails
   `date > $templogdirectory/debug.log`;
   $rtloutput = `$rtl433 $rtloptions 2>>$templogdirectory/debug.log`;
   print LOGFILE "$rtloutput";
+  $endtime=time();
+  $runtime = $endtime - $starttime;
+  if ( $runtime < 20 ) # short session, they should last ~60sec
+  {
+    $shortsessions = $shortsessions + 1;
+    print LOGFILE "\nERROR: SHORT SESSION\n";  
+  }
+  print LOGFILE "short (error) sessions since last restart: $shortsessions\n";
   close LOGFILE;
   `find $templogdirectory -type f -size +0c -exec mv {} $logdirectory \\;`;
 }
