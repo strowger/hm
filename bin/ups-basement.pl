7ba92a36da76c659d45d261d1bb05f4ce95c70c2
strowger <strowger@ls12.net>
2018-04-09 20:06:49 +0100

    ups-basement.pl: initial support for influxdb

--- bin/ups-basement.pl
+++ bin/ups-basement.pl
@@ -5,11 +5,17 @@
 
 # GH 2017-10-29
 # begun
+# GH 2018-04-08
+# influxdb added
 
 $logdirectory="/data/hm/log";
 #$logfile="ups-basement.log";
 #$rrddirectory="/data/hm/rrd";
 $lockfile="/tmp/ups-basement.lock";
+
+$influxcmd="curl -s -S -i -XPOST ";
+$influxurl="http://localhost:8086/";
+$influxdb="styes_power";
 
 if ( -f $lockfile )
 {
@@ -50,21 +56,27 @@ open BATTERYCHARGE, ">>", "$logdirectory/upsb-batterycharge.log" or die $!;
 print BATTERYCHARGE "$timestamp $batterycharge\n";
 close BATTERYCHARGE;
 
+`${influxcmd} '${influxurl}write?db=${influxdb}' --data-binary 'ups_basement_charge value=${batterycharge} ${timestamp}000000000\n'`;
+
 open BATTERYVOLTS, ">>", "$logdirectory/upsb-batteryvolts.log" or die $!;
 print BATTERYVOLTS "$timestamp $batteryvolts\n";
 close BATTERYVOLTS;
+`${influxcmd} '${influxurl}write?db=${influxdb}' --data-binary 'ups_basement_battery_volts value=${batteryvolts} ${timestamp}000000000\n'`;
 
 open MAINSFREQ, ">>", "$logdirectory/upsb-mainsfreq.log" or die $!;
 print MAINSFREQ "$timestamp $mainsfreq\n";
 close MAINSFREQ;
+`${influxcmd} '${influxurl}write?db=${influxdb}' --data-binary 'ups_basement_mains_freq value=${mainsfreq} ${timestamp}000000000\n'`;
 
 open MAINSVOLTS, ">>", "$logdirectory/upsb-mainsvolts.log" or die $!;
 print MAINSVOLTS "$timestamp $mainsvolts\n";
 close MAINSVOLTS;
+`${influxcmd} '${influxurl}write?db=${influxdb}' --data-binary 'ups_basement_mains_volts value=${mainsvolts} ${timestamp}000000000\n'`;
 
 open OUTPUTVOLTS, ">>", "$logdirectory/upsb-outputvolts.log" or die $!;
 print OUTPUTVOLTS "$timestamp $outputvolts\n";
 close OUTPUTVOLTS;
+`${influxcmd} '${influxurl}write?db=${influxdb}' --data-binary 'ups_basement_output_volts value=${outputvolts} ${timestamp}000000000\n'`;
 
 open UPSLOAD, ">>", "$logdirectory/upsb-upsload.log" or die $!;
 print UPSLOAD "$timestamp $upsload\n";
